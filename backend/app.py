25a01e73795bb22b836d542a6454ac40c36863bb
hepzanechen <62514411+hepzanechen@users.noreply.github.com>
2024-10-31 12:59:34 +0000

    update flask to gunicorn

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask_cors import CORS
 from routes import markov
+import os
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS
 app.register_blueprint(markov.bp)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Use the port assigned by Heroku, or default to 5000 for local testing
+    port = int(os.environ.get('PORT', 5000))
+    # Run with the correct host and port for production
+    app.run(host='0.0.0.0', port=port)
